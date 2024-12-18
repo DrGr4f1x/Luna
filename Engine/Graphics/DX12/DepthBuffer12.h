6cdae17128373ce4e60e48a8ef97b5043a7e8a5e
Dave Elder <elddm1@gmail.com>
2024-12-18 12:57:19 -0800

    DepthBuffer constructor.

--- Engine/Graphics/DX12/DepthBuffer12.h
+++ Engine/Graphics/DX12/DepthBuffer12.h
@@ -19,11 +19,56 @@ using namespace Microsoft::WRL;
 namespace Luna::DX12
 {
 
+struct DepthBufferDescExt
+{
+	DepthBufferDescExt()
+	{
+		for (auto& handle : dsvHandles)
+		{
+			handle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+		}
+		depthSrvHandle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+		stencilSrvHandle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+	}
+
+	ID3D12Resource* resource{ nullptr };
+	ResourceState usageState{ ResourceState::Undefined };
+	uint8_t planeCount{ 1 };
+
+	std::array<D3D12_CPU_DESCRIPTOR_HANDLE, 4> dsvHandles{};
+	D3D12_CPU_DESCRIPTOR_HANDLE depthSrvHandle{};
+	D3D12_CPU_DESCRIPTOR_HANDLE stencilSrvHandle{};
+
+	constexpr DepthBufferDescExt& SetResource(ID3D12Resource* value) noexcept { resource = value; return *this; }
+	constexpr DepthBufferDescExt& SetUsageState(ResourceState value) noexcept { usageState = value; return *this; }
+	constexpr DepthBufferDescExt& SetPlaneCount(uint8_t value) noexcept { planeCount = value; return *this; }
+	DepthBufferDescExt& SetDsvHandles(const std::array<D3D12_CPU_DESCRIPTOR_HANDLE, 4>& value) noexcept { dsvHandles = value; return *this; }
+	constexpr DepthBufferDescExt& SetDepthSrvHandle(D3D12_CPU_DESCRIPTOR_HANDLE value) noexcept { depthSrvHandle = value; return *this; }
+	constexpr DepthBufferDescExt& SetStencilSrvHandle(D3D12_CPU_DESCRIPTOR_HANDLE value) noexcept { stencilSrvHandle = value; return *this; }
+};
+
+
+class __declspec(uuid("232C36B6-60FC-4DE6-8AD5-84769D22CDFF")) IDepthBuffer12 : public IDepthBuffer
+{
+public:
+	virtual ~IDepthBuffer12() = default;
+
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDSV() const noexcept = 0;
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDSV_DepthReadOnly() const noexcept = 0;
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDSV_StencilReadOnly() const noexcept = 0;
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDSV_ReadOnly() const noexcept = 0;
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDepthSRV() const noexcept = 0;
+	virtual D3D12_CPU_DESCRIPTOR_HANDLE GetStencilSRV() const noexcept = 0;
+};
+
+
 class __declspec(uuid("36E0E19C-7D07-46EA-A6FE-E222A083957D")) DepthBuffer
-	: RuntimeClass<RuntimeClassFlags<ClassicCom>, ChainInterfaces<IDepthBuffer, IPixelBuffer, IGpuImage>>
+	: public RuntimeClass<RuntimeClassFlags<ClassicCom>, ChainInterfaces<IDepthBuffer12, IDepthBuffer, IPixelBuffer, IGpuImage>>
 	, NonCopyable
 {
 public:
+	DepthBuffer(const DepthBufferDesc& desc, const DepthBufferDescExt& descExt) noexcept;
+
 	// IGpuResource implementation
 	NativeObjectPtr GetNativeObject(NativeObjectType nativeObjectType) const noexcept final;
 
@@ -38,7 +83,13 @@ public:
 	uint32_t GetPlaneCount() const noexcept override { return m_planeCount; }
 	TextureDimension GetDimension() const noexcept override { return ResourceTypeToTextureDimension(m_resourceType); }
 
+	// IDEpthBuffer implementation
+	float GetClearDepth() const noexcept final { return m_clearDepth; }
+	uint8_t GetClearStencil() const noexcept final { return m_clearStencil; }
+
 private:
+	std::string m_name;
+
 	ComPtr<ID3D12Resource> m_resource;
 	ResourceState m_usageState{ ResourceState::Undefined };
 	ResourceState m_transitioningState{ ResourceState::Undefined };
@@ -52,6 +103,15 @@ private:
 	uint32_t m_numSamples{ 1 };
 	uint32_t m_planeCount{ 1 };
 	Format m_format{ Format::Unknown };
+
+	// DepthBuffer data
+	float m_clearDepth{ 1.0f };
+	uint8_t m_clearStencil{ 0 };
+
+	// DepthBuffer12 data
+	std::array<D3D12_CPU_DESCRIPTOR_HANDLE, 4> m_dsvHandles{};
+	D3D12_CPU_DESCRIPTOR_HANDLE m_depthSrvHandle{};
+	D3D12_CPU_DESCRIPTOR_HANDLE m_stencilSrvHandle{};
 };
 
 } // namespace Luna::DX12
