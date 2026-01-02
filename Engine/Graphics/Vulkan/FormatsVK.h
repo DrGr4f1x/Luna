6ea26f33a2ab573a1d1bc2db2692dd014fbf1029
Dave Elder <elddm1@gmail.com>
2026-01-02 06:13:41 -0800

    Removing redundant code in FormatsVK.h.

--- Engine/Graphics/Vulkan/FormatsVK.h
+++ Engine/Graphics/Vulkan/FormatsVK.h
@@ -25,10 +25,9 @@ VkImageAspectFlags GetImageAspect(Format format);
 
 inline bool IsDepthFormat(VkFormat format)
 {
-	return format == VK_FORMAT_D16_UNORM || 
-		format == VK_FORMAT_D24_UNORM_S8_UINT || 
-		format == VK_FORMAT_D32_SFLOAT || 
-		format == VK_FORMAT_D32_SFLOAT_S8_UINT || 
+	return format == VK_FORMAT_D16_UNORM ||
+		format == VK_FORMAT_D24_UNORM_S8_UINT ||
+		format == VK_FORMAT_D32_SFLOAT ||
 		format == VK_FORMAT_D32_SFLOAT_S8_UINT;
 }
 
